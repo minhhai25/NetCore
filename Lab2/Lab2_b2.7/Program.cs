000cbe05bdb72e49f5a5f5339f29e7cb021bd0ea
minhhai25 <ntmhai25@gmail.com>
2024-12-13 16:38:39 +0700

    lab2

---
+++ Lab2/Lab2_b2.7/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab2_b2._7
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 1; i <= 10; i++)
+			{
+
+				for (int k = 1; k >= 0; k--)
+				{
+					if (i != k)
+					{
+						Console.Write(" ");
+
+					}
+					else
+					{
+						Console.Write()
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
