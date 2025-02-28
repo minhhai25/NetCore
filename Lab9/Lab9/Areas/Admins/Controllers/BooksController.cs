d8cfe7446701cefe67f22c677aa0dd218a65af42
minhhai25 <ntmhai25@gmail.com>
2025-02-28 13:54:21 +0700

    Lab9 netcorre

---
+++ Lab9/Lab9/Areas/Admins/Controllers/BooksController.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab9.Models;
+using Microsoft.AspNetCore.Routing.Constraints;
+using X.PagedList.Extensions;
+
+namespace Lab9.Areas.Admins.Controllers
+{
+    [Area("Admins")]
+    public class BooksController : Controller
+    {
+        private readonly BookStoreContext _context;
+
+        public BooksController(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admins/Books
+        public async Task<IActionResult> Index(string name, int page = 1)
+        {
+            var limit = 5;
+
+            ViewBag.Name = name;
+            if (string.IsNullOrEmpty(name))
+            {
+                var bookStoreContext = _context.Books.Include(p => p.Category);
+                return View(bookStoreContext.OrderBy(x => x.CategoryId).ToPagedList(page, limit));
+            }
+            else
+            {
+                var bookStoreContext = _context.Books.Where(p => p.Title.ToUpper().Contains(name.ToUpper())).Include(p => p.Category).ToPagedList(page, limit);
+                return View( bookStoreContext);
+            }
+            
+        }
+
+        // GET: Admins/Books/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.Category)
+                .Include(b => b.Publisher)
+                .FirstOrDefaultAsync(m => m.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Admins/Books/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
+            ViewData["PublisherId"] = new SelectList(_context.Publishers, "PublisherId", "PublisherId");
+            return View();
+        }
+
+        // POST: Admins/Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BookId,Title,Author,Release,Price,Description,Picture,PublisherId,CategoryId")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Any() && files[0].Length > 0)
+                {
+                    var file = files[0];
+                    var fileName = file.FileName;
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\books", fileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        book.Picture = "images/books/" + fileName;
+                    }
+                }
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", book.CategoryId);
+            ViewData["PublisherId"] = new SelectList(_context.Publishers, "PublisherId", "PublisherId", book.PublisherId);
+            return View(book);
+        }
+
+        // GET: Admins/Books/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", book.CategoryId);
+            ViewData["PublisherId"] = new SelectList(_context.Publishers, "PublisherId", "PublisherId", book.PublisherId);
+            return View(book);
+        }
+
+        // POST: Admins/Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("BookId,Title,Author,Release,Price,Description,PublisherId,CategoryId")] Book book)
+        {
+            if (id != book.BookId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var existingProduct = await _context.Books.FindAsync(id);
+                    if (existingProduct == null)
+                    {
+                        return NotFound();
+                    }
+                    existingProduct.Title = book.Title;
+                    existingProduct.CategoryId = book.CategoryId;
+                    existingProduct.Author = book.Author;
+                    existingProduct.Price = book.Price;
+                    existingProduct.Publisher = book.Publisher;
+                    existingProduct.PublisherId = book.PublisherId;
+                    existingProduct.BookId = book.BookId;
+                    var files = HttpContext.Request.Form.Files;
+                    if (files.Count > 0 && files[0].Length > 0)
+                    {
+                        var file = files[0];
+                        var fileName = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\books", fileName);
+
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                            existingProduct.Picture = "images/books/" + fileName;
+                        }
+                    }
+                    _context.Update(existingProduct);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.BookId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", book.CategoryId);
+            ViewData["PublisherId"] = new SelectList(_context.Publishers, "PublisherId", "PublisherId", book.PublisherId);
+            return View(book);
+        }
+
+        // GET: Admins/Books/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.Category)
+                .Include(b => b.Publisher)
+                .FirstOrDefaultAsync(m => m.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Admins/Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(string id)
+        {
+            return _context.Books.Any(e => e.BookId == id);
+        }
+    }
+}
---
+++ Lab9/Lab9/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Lab9.Models;
+namespace Lab9
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("BookStoreString");
+            builder.Services.AddDbContext<BookStoreContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.MapControllerRoute(
+              name: "areas",
+              pattern: " {area:exists}/{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
